5d6f40881273f0b393872878b84efe0ec62adf69
Sebastian Bastida Marin <122469079+developersbm@users.noreply.github.com>
2024-04-06 05:20:36 +0000

    Implemented live search functionality for the audio dataset created

---
+++ dataset.js
@@ -0,0 +1,74 @@
+export let audios = {
+  data: [
+    {
+      audioName: "Example1",
+      category: "Funny",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example2",
+      category: "Vehicles",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example3",
+      category: "Example",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example4",
+      category: "Animals",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example5",
+      category: "Funny",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example6",
+      category: "Funny",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example7",
+      category: "Vehicles",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example8",
+      category: "Example",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example2",
+      category: "Vehicles",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example3",
+      category: "Example",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example4",
+      category: "Animals",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example2",
+      category: "Vehicles",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example3",
+      category: "Example",
+      image: "/img/picture-example.png",
+    },
+    {
+      audioName: "Example4",
+      category: "Animals",
+      image: "/img/picture-example.png",
+    },
+  ],
+};
